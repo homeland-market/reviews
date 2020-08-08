cc1999eedc63fa7e211925171ba3860bc704a49a
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-08 10:53:04 -0700

    Changes made to server index.js to reflect Andrews suggestions.

--- server/index.js
+++ server/index.js
@@ -6,14 +6,13 @@ const PORT = 2373;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(express.static(path.join('/../public')));
 
 app.get('/api/reviews/:id', (req, res) => {
-  const requestId = req.originalUrl.substring('/api/reviews/'.length);
+  const requestId = req.params.id;
   models.getAll(requestId, (error, reviews) => {
     if (error) {
-      console.log(error);
+      console.error(error);
       res.status(502).send('There was an error with your request');
     } else {
       res.status(200).json(reviews);
