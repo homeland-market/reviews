1d0f30d12ee79b68fa59b68bfb3c87158827e7fe
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-12 12:19:51 -0700

    Refactors tests as per PR suggestions

--- test/test.js
+++ test/test.js
@@ -22,11 +22,12 @@ describe('Retrieve review data from the database', () => {
     const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
     const promises = [];
     while (id < 100) {
-      expect.assertions(100);
       db.query(queryString, [id], (err, results) => {
+        if (err) { done(err); }
         promises.push(results);
         if (promises.length === 100) {
           for (let i = 0; i < promises.length; i += 1) {
+            expect.assertions(100);
             expect(promises[i][0].comment).toBeDefined();
           }
           done();
@@ -41,11 +42,12 @@ describe('Retrieve review data from the database', () => {
     const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
     const promises = [];
     while (id < 100) {
-      expect.assertions(100);
       db.query(queryString, [id], (err, results) => {
+        if (err) { done(err); }
         promises.push(results);
         if (promises.length === 100) {
           for (let i = 0; i < promises.length; i += 1) {
+            expect.assertions(100);
             expect(promises[i][0].name).toBeDefined();
           }
           done();
@@ -60,11 +62,12 @@ describe('Retrieve review data from the database', () => {
     const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
     const promises = [];
     while (productId < 100) {
-      expect.assertions(200);
       db.query(queryString, [productId], (err, results) => {
+        if (err) { done(err); }
         promises.push(results);
         if (promises.length === 100) {
           for (let i = 0; i < promises.length; i += 1) {
+            expect.assertions(200);
             expect(promises[i][0].rating).toBeGreaterThanOrEqual(1);
             expect(promises[i][0].rating).toBeLessThan(6);
           }
@@ -80,11 +83,12 @@ describe('Retrieve review data from the database', () => {
     const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
     const promises = [];
     while (productId < 100) {
-      expect.assertions(200);
       db.query(queryString, [productId], (err, results) => {
+        if (err) { done(err); }
         promises.push(results);
         if (promises.length === 100) {
           for (let i = 0; i < promises.length; i += 1) {
+            expect.assertions(200);
             expect(promises[i][0].helpful).toBeGreaterThanOrEqual(0);
             expect(promises[i][0].helpful).toBeLessThan(16);
           }
@@ -100,11 +104,12 @@ describe('Retrieve review data from the database', () => {
     const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
     const promises = [];
     while (productId < 100) {
-      expect.assertions(100);
       db.query(queryString, [productId], (err, results) => {
+        if (err) { done(err); }
         promises.push(results);
         if (promises.length === 100) {
           for (let i = 0; i < promises.length; i += 1) {
+            expect.assertions(100);
             expect((new Date(promises[i][0].date)) < (new Date())).toBeTruthy();
           }
           done();
@@ -119,11 +124,12 @@ describe('Retrieve review data from the database', () => {
     const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
     const promises = [];
     while (productId < 100) {
-      expect.assertions(100);
       db.query(queryString, [productId], (err, results) => {
+        if (err) { done(err); }
         promises.push(results);
         if (promises.length === 100) {
           for (let i = 0; i < promises.length; i += 1) {
+            expect.assertions(100);
             expect(promises[i][0].location).toBeDefined();
           }
           done();
