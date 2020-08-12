e94cbe468e52a552507898650b8b3d23cd770732
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-12 00:15:29 -0700

    Implements basic tests for database information retrieval

--- test/test.js
+++ test/test.js
@@ -1,29 +1,30 @@
 const mysql = require('mysql');
 const mysqlConfig = require('../server/database/config');
 
-const db = mysql.createConnection(mysqlConfig);
+describe('Retrieve review data from the database', () => {
+  const db = mysql.createConnection(mysqlConfig);
 
-beforeEach(async (done) => {
-  await db.connect((err) => {
-    if (err) {
-      done(err);
-    } else {
-      done();
-    }
+  beforeAll(async (done) => {
+    await db.connect((err) => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
   });
-});
 
-afterEach(async (done) => {
-  await db.destroy();
-  done();
-});
+  afterAll(async (done) => {
+    await db.end();
+    done();
+  });
 
-describe('Database interactions', () => {
-  test('Should get review comments for all product id', async (done) => {
+  test('Should get comments for all product id entries', (done) => {
     let id = 0;
     const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
     const promises = [];
     while (id < 100) {
+      expect.assertions(100);
       db.query(queryString, [id], (err, results) => {
         promises.push(results);
         if (promises.length === 100) {
@@ -34,6 +35,98 @@ describe('Database interactions', () => {
         }
       });
       id += 1;
+    }
+  });
+  test('Should get names for all product id entries', (done) => {
+    let id = 0;
+    const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
+    const promises = [];
+    while (id < 100) {
+      expect.assertions(100);
+      db.query(queryString, [id], (err, results) => {
+        promises.push(results);
+        if (promises.length === 100) {
+          for (let i = 0; i < promises.length; i += 1) {
+            expect(promises[i][0].name).toBeDefined();
+          }
+          done();
+        }
+      });
+      id += 1;
+    }
+  });
+  test('Should get ratings for all product id entries & should be between 1-5', (done) => {
+    let productId = 0;
+    const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
+    const promises = [];
+    while (productId < 100) {
+      expect.assertions(200);
+      db.query(queryString, [productId], (err, results) => {
+        promises.push(results);
+        if (promises.length === 100) {
+          for (let i = 0; i < promises.length; i += 1) {
+            expect(promises[i][0].rating).toBeGreaterThanOrEqual(1);
+            expect(promises[i][0].rating).toBeLessThan(6);
+          }
+          done();
+        }
+      });
+      productId += 1;
+    }
+  });
+  test('Should get helpful scores for all product id entries & should be between 0-15', (done) => {
+    let productId = 0;
+    const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
+    const promises = [];
+    while (productId < 100) {
+      expect.assertions(200);
+      db.query(queryString, [productId], (err, results) => {
+        promises.push(results);
+        if (promises.length === 100) {
+          for (let i = 0; i < promises.length; i += 1) {
+            expect(promises[i][0].helpful).toBeGreaterThanOrEqual(0);
+            expect(promises[i][0].helpful).toBeLessThan(16);
+          }
+          done();
+        }
+      });
+      productId += 1;
+    }
+  });
+  test('Should get all dates for product id entries & all dates should be in the past', (done) => {
+    let productId = 0;
+    const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
+    const promises = [];
+    while (productId < 100) {
+      expect.assertions(100);
+      db.query(queryString, [productId], (err, results) => {
+        promises.push(results);
+        if (promises.length === 100) {
+          for (let i = 0; i < promises.length; i += 1) {
+            expect((new Date(promises[i][0].date)) < (new Date())).toBeTruthy();
+          }
+          done();
+        }
+      });
+      productId += 1;
+    }
+  });
+  test('Should get all locations for product id entries', (done) => {
+    let productId = 0;
+    const queryString = 'SELECT * FROM user_reviews WHERE url_id = ?';
+    const promises = [];
+    while (productId < 100) {
+      expect.assertions(100);
+      db.query(queryString, [productId], (err, results) => {
+        promises.push(results);
+        if (promises.length === 100) {
+          for (let i = 0; i < promises.length; i += 1) {
+            expect(promises[i][0].location).toBeDefined();
+          }
+          done();
+        }
+      });
+      productId += 1;
     }
   });
 });
