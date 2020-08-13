472d186986e847e38dcecf7c7e399c16fd3050b3
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-12 21:50:31 -0700

    Fixes import spacing

--- client/components/HelpfulButton.jsx
+++ client/components/HelpfulButton.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import { updateHelpfulCount } from '../lib/databaseRequests';
 
 class HelpfulButton extends Component {
