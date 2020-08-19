d47ab0a0610cd666281c68e323b3720cb1e23486
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-19 16:18:01 -0700

    Updates css ordering

--- src/components/LoadingPlaceHolder/BarsLoadingPlaceholder.jsx
+++ src/components/LoadingPlaceHolder/BarsLoadingPlaceholder.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LoadingBars = styled.div`
-  width: 100%;
-  padding-bottom: 3%;
-  margin-top: 12px;
-  border-radius: 8px;
-  background: #8f82a6;
   animation: fadein .8s infinite alternate;
+  background: #8f82a6;
+  border-radius: 8px;
+  margin-top: 12px;
+  padding-bottom: 3%;
+  width: 100%;
 
   @keyframes fadein {
     from { opacity: 1; }
--- src/components/LoadingPlaceHolder/MainLoadingPlaceholder.jsx
+++ src/components/LoadingPlaceHolder/MainLoadingPlaceholder.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MainLoadingBlock = styled.div`
-  width: 100%;
-  margin-top: 12px;
-  margin-bottom: 12px;
-  padding-bottom: 20%;
-  border-radius: 8px;
-  background: #8f82a6;
   animation: fadein .8s infinite alternate;
+  background: #8f82a6;
+  border-radius: 8px;
+  margin-bottom: 12px;
+  margin-top: 12px;
+  padding-bottom: 20%;
+  width: 100%;
 
   @keyframes fadein {
     from { opacity: 1; }
