ae210837291260bc9fba487f4aa063460cc053f3
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-20 02:51:25 -0700

    Fixes seeding script to also build the database!

---
+++ server/database/createDatabaseTables.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-param-reassign */
+module.exports = (db) => {
+  if (!db.queryAsync) {
+    db = Promise.promisifyAll(db);
+  }
+  return db.queryAsync(`
+  CREATE TABLE IF NOT EXISTS user_reviews (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    url_id INT NOT NULL,
+    name VARCHAR(15) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    date DATETIME NOT NULL,
+    comment VARCHAR(8000) NOT NULL,
+    rating INT NOT NULL,
+    helpful INT DEFAULT 0,
+    img VARCHAR(225),
+    imgmedium VARCHAR(225)
+  );`)
+    .catch((err) => { console.log(err); });
+};
--- server/database/index.js
+++ server/database/index.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-console */
 const mysql = require('mysql');
+const Promise = require('bluebird');
 const mysqlConfig = require('./config.js');
+const createTables = require('./createDatabaseTables.js');
+const databaseSeeder = require('./dbSeed.js');
 
-const dbConnection = mysql.createConnection(mysqlConfig);
+const database = 'reviews';
+const connection = mysql.createConnection(mysqlConfig);
 
-dbConnection.connect((err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('🚀🚀 review database connected!');
-  }
-});
+const db = Promise.promisifyAll(connection, { multiArgs: true });
 
-module.exports = dbConnection;
+db.connectAsync()
+  .then(() => console.log('\x1b[33m', '🆅', '\x1b[37m', `${database} database connected!`))
+  .then(() => db.queryAsync(`DROP DATABASE IF EXISTS ${database}`))
+  .then(() => db.queryAsync(`CREATE DATABASE IF NOT EXISTS ${database}`))
+  .then(() => db.queryAsync(`USE ${database}`))
+  .then(() => createTables(db))
+  .then(() => databaseSeeder());
+
+module.exports = db;
