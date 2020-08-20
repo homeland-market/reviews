bce01bccff7090a07b1f9f75fa9e3f5df47b4308
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-08-19 20:22:00 -0700

    Adds extra CORS for PUT requests

--- src/components/LoadingPlaceHolder/MainLoadingPlaceholder.jsx
+++ src/components/LoadingPlaceHolder/MainLoadingPlaceholder.jsx
@@ -10,8 +10,8 @@ const MainLoadingBlock = styled.div`
   border-radius: 8px;
   display: block;
   margin: 12px auto;
-  padding-bottom: 5%;
-  padding-top: 5%;
+  padding-bottom: 3%;
+  padding-top: 3%;
   text-align: center;
   width: 100%;
 
@@ -26,7 +26,7 @@ const MainLoadingPlaceholder = () => (
     <MainLoadingBlock>
       <div>
         <StarRatings
-          starDimension="10vw"
+          starDimension="5vw"
           starSpacing="2rem"
           starEmptyColor="#d9d8db"
           svgIconPath={SmallStarSVGPath}
